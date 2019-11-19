c2df9014c0d500f63e51c3edcbd2d07a6264abb6
Alexander Tatchin <alexander.tatchin@gmail.com>
2019-11-19 04:28:30 +0300

    Add __version__ variable.

--- givemedata/__init__.py
+++ givemedata/__init__.py
@@ -2,10 +2,13 @@ from .givemedata import dict_config
 from .givemedata import Data
 from .givemedata import get_provider_from_config
 from . import utils
+from .version import VERSION as __version__
+
 
 __all__ = [
     dict_config,
     Data,
     get_provider_from_config,
     utils,
+    __version__,
 ]
