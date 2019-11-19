f3ef049f8b47023a0ff7673270cae2d094de781f
Alexander Tatchin <alexander.tatchin@gmail.com>
2019-11-19 03:58:57 +0300

    Fix __all__ in givemedata module.

--- givemedata/__init__.py
+++ givemedata/__init__.py
@@ -1,3 +1,11 @@
 from .givemedata import dict_config
 from .givemedata import Data
 from .givemedata import get_provider_from_config
+from . import utils
+
+__all__ = [
+    dict_config,
+    Data,
+    get_provider_from_config,
+    utils,
+]
